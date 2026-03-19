baac68ff486bc7af1ea20c36d238b8b0507106c4
AlizwaMbane <alizwambane@gmail.com>
2026-03-19 21:03:19 +0200

    Update courseoffered.java

--- src/main/java/com/example/demo2/courseoffered.java
+++ src/main/java/com/example/demo2/courseoffered.java
@@ -1,1 +1,23 @@
+package cim.example.demo2;
 
+public class CourseOffered {
+
+  private String name;
+  private String level;
+
+  // Constructore
+  public CourseOffered(String name, String level) {
+      this.name = name;
+      this.name = level;
+  }
+
+  // Getter for name
+  public String getName() {
+    return name;
+  }
+  
+  // Getter for level
+  public String getlevel() {
+    return level;
+  }
+  }
